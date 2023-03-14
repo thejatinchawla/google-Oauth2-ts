a65cf02f2c0ed86cc12a08965573b478dba7bcc1
Jatin Chawla <106448198+thejatinchawla@users.noreply.github.com>
2023-03-14 23:29:30 +0530

    Add files via upload

---
+++ server/src/config.ts
@@ -0,0 +1,9 @@
+export const GOOGLE_CLIENT_ID =
+  process.env.GOOGLE_CLIENT_ID ||
+  "544624534401-0l8089urh3e9oeg8i95qrr8f4djmns2j.apps.googleusercontent.com";
+export const GOOGLE_CLIENT_SECRET =
+  process.env.GOOGLE_CLIENT_SECRET || "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";
+export const SERVER_ROOT_URI = "http://localhost:4000";
+export const UI_ROOT_URI = "http://localhost:3000";
+export const JWT_SECRET = "shhhhh";
+export const COOKIE_NAME = "auth_token";
